ab6d99335f68d302600666a77b452305f72a505a
Sebastian Reitenbach <sebastia@l00-bugdead-prods.de>
2014-09-07 22:44:31 +0200

    A few new templates, for the new classes sharednetwork and subnet.

---
+++ manifests/sharednetwork.pp
@@ -0,0 +1,23 @@
+# ----------
+# Shared Network
+# ----------
+define dhcpd::sharednetwork (
+  $name,
+) {
+
+  include dhcpd::params
+
+  $config_file = $dhcpd::params::config_file
+
+  concat::fragment { "dhcpd_sharednetwork-header_${name}":
+    target  => $config_file,
+    content => template('dhcpd/dhcpd.sharednetwork-header.erb'),
+    order   => 5,
+  }
+  concat::fragment { "dhcpd_sharednetwork-footer_${name}":
+    target  => $config_file,
+    content => template('dhcpd/dhcpd.sharednetwork-footer.erb'),
+    order   => 100,
+  }
+}
+
---
+++ manifests/subnet.pp
@@ -0,0 +1,28 @@
+# ----------
+# Subnet 
+# ----------
+define dhcpd::subnet (
+  $network,
+  $netmask,
+  $routers = undef,
+  $range = undef,
+  $options = '',
+  $parameters = '',
+) {
+
+  include dhcpd::params
+
+  $config_file = $dhcpd::params::config_file
+
+  concat::fragment { "dhcpd_subnet-header_${name}":
+    target  => $config_file,
+    content => template('dhcpd/dhcpd.subnet-header.erb'),
+    order   => 5,
+  }
+  concat::fragment { "dhcpd_subnet-footer_${name}":
+    target  => $config_file,
+    content => template('dhcpd/dhcpd.subnet-footer.erb'),
+    order   => 90,
+  }
+}
+
